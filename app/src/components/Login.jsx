07aa30a25fc86b6cc501185f651e2e492e82078b
sweta-chaudhary <swetachaudhary014@gmail.com>
2023-10-06 22:06:08 +0530

    new commit

--- app/src/components/Login.jsx
+++ app/src/components/Login.jsx
@@ -3,78 +3,81 @@ import { Link } from 'react-router-dom';
 import  './css/login.css';
 
 
-class Login extends Component {
-  constructor() {
-    super();
-    this.state = {
-      role: 'user', // Default role is 'user'
-      username: '',
-      password: '',
-    };
-  }
+import React, { useState } from 'react';
 
-  handleRoleChange = (event) => {
-    this.setState({ role: event.target.value });
+function Login() {
+  const [role, setRole] = useState('user');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRoleChange = (event) => {
+    setRole(event.target.value);
   };
 
-  handleInputChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    if (name === 'username') {
+      setUsername(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
   };
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     // Here, you can implement your login logic based on the selected role.
     // For demonstration purposes, we're just printing the input values.
-    console.log('Role:', this.state.role);
-    console.log('Username:', this.state.username);
-    console.log('Password:', this.state.password);
+    console.log('Role:', role);
+    console.log('Username:', username);
+    console.log('Password:', password);
   };
 
-  render() {
-    return (
-      <div className="login-container">
-        <h1>Login</h1>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="role">Select Role:</label>
-            <select
-              id="role"
-              name="role"
-              value={this.state.role}
-              onChange={this.handleRoleChange}
-            >
-              <option value="user">User</option>
-              <option value="admin">Admin</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              value={this.state.username}
-              onChange={this.handleInputChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleInputChange}
-              required
-            />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-        <span> Don't have an account? <a href="/register">Register</a></span>
-      </div>
-    );
-  }
+  return (
+    <div className="login-container">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="role">Select Role:</label>
+          <select
+            id="role"
+            name="role"
+            value={role}
+            onChange={handleRoleChange}
+          >
+            <option value="user">User</option>
+            <option value="admin">Admin</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <span>
+        {' '}
+        Don't have an account? <a href="/register">Register</a>
+      </span>
+    </div>
+  );
 }
 
 export default Login;
